b619136354d1dbb0c3be23afa1d2fc5be4a77a0a
Luardino Eduardo Lopes Gaspar Jaime <luardino@icloud.com>
2022-12-13 17:32:10 +0100

    This is the first version, the game is 40% completed, only player x selection works and the game opens.

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+const selectBox = document.querySelector('.select-box')
+selectXBtn = selectBox.querySelector('.playerX')
+selectOBtn = selectBox.querySelector('.playerO')
+playBoard = document.querySelector('.play-board')
+
+window.onload = ()=>{
+    selectXBtn.onclick = () =>{
+        selectBox.classList.add('hide')
+        playBoard.classList.add('show')
+    }
+}
