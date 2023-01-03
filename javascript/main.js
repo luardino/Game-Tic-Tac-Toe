0fa27415574b171589fb013a73b4b273c4833f88
Luardino Eduardo Lopes Gaspar Jaime <luardino@icloud.com>
2023-01-03 23:59:30 +0100

    .

--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,15 @@ const selectBox = document.querySelector('.select-box')
 selectXBtn = selectBox.querySelector('.playerX')
 selectOBtn = selectBox.querySelector('.playerO')
 playBoard = document.querySelector('.play-board')
+allBox = document.querySelectorAll('section span')
+players = document.querySelector('.players')
 
 window.onload = ()=>{
+
+    for(i = 0; i < allBox.length; i++){
+        allBox[i].setAttribute('onclick', 'clickedBox(this)')
+    }
+
     selectXBtn.onclick = () =>{
         selectBox.classList.add('hide')
         playBoard.classList.add('show')
@@ -12,5 +19,13 @@ window.onload = ()=>{
     selectOBtn.onclick = ()=>{
         selectBox.classList.add('hide')
         playBoard.classList.add('show')
+        players.setAttribute('class', 'players active')
     }
+}
+
+let playerXicon = 'fas fa-times'
+let playerOicon = 'fas fa-circle'
+
+function clickedBox(element){
+    
 }
