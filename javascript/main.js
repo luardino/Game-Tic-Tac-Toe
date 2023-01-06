ea2abee7d22fa889c663bf060e6d5f04f7ebd791
Luardino Eduardo Lopes Gaspar Jaime <luardino@icloud.com>
2023-01-06 23:42:28 +0100

    .

--- javascript/main.js
+++ javascript/main.js
@@ -19,14 +19,15 @@ window.onload = ()=>{
     selectOBtn.onclick = ()=>{
         selectBox.classList.add('hide')
         playBoard.classList.add('show')
-        players.setAttribute('class', 'players active player')
+        players.setAttribute("class", 'players active player')
     }
 }
 
-let playerXIcon = 'fas fa-times'
-let playerOIcon = 'far fa-circle'
+let playerXIcon = "fas fa-times"
+let playerOIcon = "fa-regular fa-circle"
 
 function clickedBox(element){
+    console.log(element);
     if(players.classList.contains('player')){
         element.innertHTML = `<i class = '${playerOIcon}'></i>`
     }else{
