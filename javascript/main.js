6408fe845868d54ca4319752d546752c0910a4b8
Luardino Jaime <luardino@icloud.com>
2023-02-13 23:22:14 +0100

    add limited on a box

--- javascript/main.js
+++ javascript/main.js
@@ -26,11 +26,23 @@ window.onload = ()=>{
 let playerXIcon = "fas fa-times"
 let playerOIcon = "fa-regular fa-circle"
 
+
 function clickedBox(element){
-    console.log(element);
+    console.log(element)
     if(players.classList.contains('player')){
-        element.innertHTML = `<i class = '${playerOIcon}'></i>`
+        element.innertHTML = `<i class = "${playerOIcon}"></i>`;
+        players.classList.add("active")
     }else{
         element.innertHTML = `<i class = '${playerXIcon}'</i>`
+        players.classList.add("active")
+    }
+    element.style.pointerEvents = 'none';
+}
+
+function bot(){
+    let array = [];
+    for(let i = 0; i < allBox.length; i++){
+        if(allBox[i].childElementCount == 0){
+            array.push(i)
     }
 }
