45e79e896dae3d7d0840f54d36c96760212e022b
Luardino Jaime <luardino@icloud.com>
2023-02-14 23:38:18 +0100

    Solved the bug that did not mark the spaces with X

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,12 @@
 const selectBox = document.querySelector('.select-box')
-selectXBtn = selectBox.querySelector('.playerX')
-selectOBtn = selectBox.querySelector('.playerO')
-playBoard = document.querySelector('.play-board')
-allBox = document.querySelectorAll('section span')
-players = document.querySelector('.players')
+const selectXBtn = selectBox.querySelector('.playerX')
+const selectOBtn = selectBox.querySelector('.playerO')
+const playBoard = document.querySelector('.play-board')
+const allBox = document.querySelectorAll('section span')
+const players = document.querySelector('.players')
 
-window.onload = ()=>{
-
-    for(i = 0; i < allBox.length; i++){
+window.onload = () => {
+    for(let i = 0; i < allBox.length; i++){
         allBox[i].setAttribute('onclick', 'clickedBox(this)')
     }
 
@@ -30,19 +29,19 @@ let playerOIcon = "fa-regular fa-circle"
 function clickedBox(element){
     console.log(element)
     if(players.classList.contains('player')){
-        element.innertHTML = `<i class = "${playerOIcon}"></i>`;
+        element.innerHTML = `<i class = "${playerOIcon}"></i>`
         players.classList.add("active")
     }else{
-        element.innertHTML = `<i class = '${playerXIcon}'</i>`
+        element.innerHTML = `<i class = "${playerXIcon}"></i>`
         players.classList.add("active")
     }
-    element.style.pointerEvents = 'none';
+    element.style.pointerEvents = 'none'
 }
 
-function bot(){
+/*function bot(){
     let array = [];
     for(let i = 0; i < allBox.length; i++){
         if(allBox[i].childElementCount == 0){
             array.push(i)
     }
-}
+*/
